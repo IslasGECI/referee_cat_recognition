ec799d4787d3d81ec6b9e6c375af2dfeeda85755
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-01-09 20:21:59 +0000

    ♻️  Refactor

--- referee/referee.py
+++ referee/referee.py
@@ -2,12 +2,12 @@ def add_offset(augend: int, addend: int) -> int:
     return augend + addend
 
 
-def calculate_true_positive(cat_detected, with_cat):
+def calculate_true_positive(cat_detected, with_cat, all_data):
     return cat_detected & with_cat
 
 
 def xxcalculate_true_positive(cat_detected, with_cat):
-    return calculate_true_positive(cat_detected, with_cat)
+    return calculate_true_positive(cat_detected, with_cat, None)
 
 
 def calculate_true_negative(cat_not_detected, without_cat):
