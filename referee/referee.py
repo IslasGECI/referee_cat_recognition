dfaf3e908e697704cb12baa53f1b3b734ce1b4ce
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-01-10 22:44:29 +0000

    ♻️  Refactor

--- referee/referee.py
+++ referee/referee.py
@@ -9,7 +9,7 @@ def calculate_true_negative(all_data):
 
 
 def calculate_false_positive(cat_detected, with_cat):
-    all_data = None
+    all_data = {"cat_detected": cat_detected, "with_cat": with_cat}
     return xxcalculate_false_positive(all_data, cat_detected, with_cat)
 
 
