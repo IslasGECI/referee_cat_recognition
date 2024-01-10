0daa39fdbad7554d0ec21a9d84b46417da65dd5c
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-01-10 22:28:53 +0000

    ♻️  Refactor

--- referee/referee.py
+++ referee/referee.py
@@ -8,6 +8,10 @@ def calculate_true_negative(cat_not_detected, without_cat):
     return cat_not_detected & without_cat
 
 
+def xxcalculate_true_negative(cat_not_detected, without_cat):
+    return cat_not_detected & without_cat
+
+
 def calculate_false_positive(cat_detected, with_cat):
     return cat_detected - with_cat
 
