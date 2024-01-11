5e0aa2e2431bec59f9b2f09a7ca82ede0caf5c60
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-01-11 21:16:49 +0000

    ♻️  Refactor

--- referee/referee.py
+++ referee/referee.py
@@ -21,15 +21,15 @@ def calculate_total(all_data):
     true_negative = calculate_true_negative(all_data)
     false_positive = calculate_false_positive(all_data)
     false_negative = calculate_false_negative(all_data)
-    all_data = {
+    binary_classification = {
         "true_positive": true_positive,
         "true_negative": true_negative,
         "false_positive": false_positive,
         "false_negative": false_negative,
     }
     return len(
-        all_data["true_positive"]
-        | all_data["true_negative"]
-        | all_data["false_positive"]
-        | all_data["false_negative"]
+        binary_classification["true_positive"]
+        | binary_classification["true_negative"]
+        | binary_classification["false_positive"]
+        | binary_classification["false_negative"]
     )
